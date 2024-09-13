58224d8ca04b6de2afbc4ec7e21d0639a2587369
yusufashry <yusuf.ashry@student.lssc.edu>
2024-09-13 18:46:59 -0400

    Create YummyMoto2.java

---
+++ YummyMoto2.java
@@ -0,0 +1,12 @@
+public class YummyMoto {
+  public static void main {String[] args) {
+    //Define 
+    String motto = "Yummy makes the food that makes it a party";
+    //Length
+    int borderlength = motto.length() +4;
+    printBorder(borderlength);
+    
+     System.out.println("*" + motto + "*");
+    
+    printBorder(borderlength);
+  }
